337fc5aa15f88708f3e5cbd430d30e6aa6172595
Селедцов Вячеслав <thisburninglife@gmail.com>
2020-03-18 01:32:01 +0300

    Create a customer with full name, day of birth, address and a rating class which defaults to ‚2‘.

---
+++ MyBank/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBank.Entities
+{
+    public class Customer
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; } = 2;
+    }
+}
